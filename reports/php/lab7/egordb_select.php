1eb836223dfab370eaec09f930e663e14cf21f8b
egortitov <70881645+egortitov@users.noreply.github.com>
2021-12-02 09:45:03 +0200

    egor_select

--- reports/php/lab7/egordb_select.php
+++ reports/php/lab7/egordb_select.php
@@ -1,13 +1,15 @@
 <?php
 $connection = new mysqli("localhost:3306", "root", "", "laba7");
-$query1 = "SELECT id_photo, id_archive FROM adding";
-$adding = $connection->query($query1);
-foreach ($adding as $row){
-    $query3 = "SELECT * FROM photos WHERE id = ". $row['id_photo']. "AND id_archive =" .$row['id_archive'];
+$query1 = "SELECT * FROM archives";
+$archives = $connection->query($query1);
+foreach ($archives as $row){
+    $query3 = "SELECT * FROM photos WHERE id_archive =" .$row['id'];
 if (!$photos = $connection->query($query3)){
  echo("Error description: photo" . $connection -> error);
-}
+} echo 'Архів фотографій №' .$row['id']. ':<ul>';
     foreach ($photos as $photo){
-       echo 'Назва фото:' .$photo["name"]. "<br>";
-    }}
+       echo 'Назва фото: ' .$photo["name"]. "<br>";
+    }
+    echo '</ul>';
+    }
 ?>
