5102b0c2fb532ccac03a53c11be758c4b0eb9551
Jedich <alexyudakov483@gmail.com>
2021-12-09 00:22:32 +0200

    return head

--- site/archive.php
+++ site/archive.php
@@ -3,7 +3,21 @@
 	global $pName;
 	$pName = "archive";
 ?>
-<?php include 'head.php' ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UkrAbobus</title>
+	<meta charset="utf-8" />
+	<link
+			href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+			rel="stylesheet"
+			integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
+			crossorigin="anonymous"
+	/>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+	<script src="js/digital_clock.js"></script>
+</head>
 <body>
 	<?php include 'header.php' ?>
 	<div class="center">
--- site/login_page.php
+++ site/login_page.php
@@ -1,5 +1,19 @@
 <?php include('./register.php') ?>
-<?php require './head.php' ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UkrAbobus</title>
+	<meta charset="utf-8" />
+	<link
+			href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+			rel="stylesheet"
+			integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
+			crossorigin="anonymous"
+	/>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+	<script src="js/digital_clock.js"></script>
+</head>
 <?php $t = ($isLogin == 'true') ? true : false; ?>
 <body>
 	<?php require './header.php' ?>
